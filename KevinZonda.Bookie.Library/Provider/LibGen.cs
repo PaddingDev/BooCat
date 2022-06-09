dfa99bef3130a3e5f60c76dea141471c19de5477
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-09 21:32:07 +0100

    feat: add LibGen base

---
+++ KevinZonda.Bookie.Library/Provider/LibGen.cs
@@ -0,0 +1,19 @@
+﻿using KevinZonda.Bookie.Library.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KevinZonda.Bookie.Library.Provider;
+public class LibGen : Provider
+{
+    protected override string _searchPrefix => "https://libgen.li/index.php?req=";
+    protected override string _baseUrl => "https://libgen.io";
+
+    protected override BookInfo[] ParseRespose(string response)
+    {
+        throw new NotImplementedException();
+    }
+}
