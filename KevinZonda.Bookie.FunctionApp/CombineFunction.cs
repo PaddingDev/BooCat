098bd2d584b60278778c615dd480322893f1398f
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-14 23:32:45 +0100

    feat: support new exp model

--- KevinZonda.Bookie.FunctionApp/CombineFunction.cs
+++ KevinZonda.Bookie.FunctionApp/CombineFunction.cs
@@ -36,15 +36,21 @@ public static partial class MainFunction
         }
         await Task.Factory.StartNew(() => Task.WaitAll(_dic.Values.ToArray(), 10000));
 
-        var _resultDic = new Dictionary<string, BookInfo[]>();
+        var _resultDic = new Dictionary<string, object>();
+        var _errDic = new Dictionary<string, ErrModel>();
         foreach (var kvp in _dic)
         {
             var rst = kvp.Value;
-            if (!rst.IsCompleted) continue;
+            if (!rst.IsCompleted)
+            {
+                _errDic.Add(kvp.Key, (ErrModel)"Not completed.");
+                continue;
+            }
             var value = rst.Result;
-            if (value.Err != null) continue;
-            _resultDic.Add(kvp.Key, value.Infos);
+            if (value.Err != null) _errDic.Add(kvp.Key, (ErrModel)value.Err);
+            else _resultDic.Add(kvp.Key, value.Infos);
         }
+        _resultDic["err"] = _errDic;
         return new OkObjectResult(_resultDic);
     }
 }
