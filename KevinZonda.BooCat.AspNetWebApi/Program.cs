a287f989c9950692ed3bcb4499be91a3275f7741
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-17 17:45:53 +0100

    feat: add Cache Base

--- KevinZonda.BooCat.AspNetWebApi/Program.cs
+++ KevinZonda.BooCat.AspNetWebApi/Program.cs
@@ -1,8 +1,18 @@
 using KevinZonda.BooCat.AspNetCoreWebAPI.Controllers;
 
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddStackExchangeRedisCache(x =>
+{
+    x.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 var app = builder.Build();
 
@@ -10,7 +20,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Map("/api/{req}", async (string req, string name, HttpContext c) =>
+app.Map("/api/{req}", async (string req, string name, HttpContext c, IMemoryCache? mem, IDistributedCache? redis) =>
 {
     if (!(req.ToLower() is "allbooks" or "all" or "a"))
         return await BooCatController.ProviderRequest(req, name);
