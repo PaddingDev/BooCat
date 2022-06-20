0da65428587a6c09d7f7b80b1c381f48f2ef5411
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-20 05:07:27 +0100

    refactor: mv to separate folder

--- script/init-ngx.sh
+++ script/init-ngx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 apt-get update & apt-get upgrade -y
-apt install nginx nano -y
+apt-get install nginx nano -y
 systemctl enable nginx
 curl https://ssl-config.mozilla.org/ffdhe2048.txt > /var/lib/nginx/dhparam.pem
 chmod +r /var/lib/nginx/dhparam.pem
 mkdir -p /data/nginx/cache
 mkdir -p /www/wwwlogs/
 mkdir -p /var/www/cert/
-mv nginx.conf /etc/nginx/sites-enabled/boocat.org.conf
+cp nginx.conf /etc/nginx/sites-enabled/boocat.org.conf
 
 echo 'TLS Cert' > /var/www/cert/bc.pem
 echo 'TLS Key' > /var/www/cert/bc.key
