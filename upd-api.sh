3c8eaa3053af25bd57c51f4a3c868a5cbccd486b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-20 05:03:03 +0100

    feat: add upd script

---
+++ upd-api.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "Fetching new version..."
+cd /var/boocat/src
+git fetch --all
+git reset --hard origin/master
+git pull
+
+echo "Stopping current services..."
+sudo systemctl stop boocat.service
+
+echo "Creating bin backup..."
+sudo mv /var/boocat/bin /var/boocat/bin.bak
+sudo mkdir /var/boocat/bin -p
+
+echo "Building new version..."
+dotnet publish KevinZonda.BooCat.AspNetWebApi -c Release -o /var/boocat/bin/
+
+if [ $? -ne 0 ]; then
+    echo "Failed to publish, falling back..."
+    sudo rm -fr /var/boocat/bin
+    sudo mv /var/boocat/bin.bak /var/boocat/bin
+    sudo systemctl start boocat.service
+    echo "Upgrade failed."
+    exit 1
+fi
+
+echo "Creating service backup..."
+sudo mv /etc/systemd/system/boocat.service /etc/systemd/system/boocat.service.bak
+
+echo "Updating service file..."
+sudo cp boocat.service /etc/systemd/system/
+
+echo "Try to start service..."
+sudo systemctl start boocat.service
+
+if [ $? -ne 0 ]; then
+    echo "Failed to start with new service, falling back service..."
+    sudo mv /etc/systemd/system/boocat.service.bak /etc/systemd/system/boocat.service
+    echo "Try to start new bin with old service..."
+    sudo systemctl start boocat.service
+    if [ $? -ne 0 ]; then
+        echo "Failed to start with old service, falling back bin..."
+        sudo mv /var/boocat/bin.bak /var/boocat/bin
+        exit 1
+    fi
+fi
+
+echo "Cleaning..."
+sudo rm -fr /var/boocat/bin.bak
+
+echo "Upgrade complete."
