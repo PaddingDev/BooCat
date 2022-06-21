da5109eb7c7f776ceeed01a99937d91bc0f1573e
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-21 13:16:03 +0100

    feat: remove console

--- KevinZonda.BooCat.AspNetWebApi/Controllers/BooCatController.cs
+++ KevinZonda.BooCat.AspNetWebApi/Controllers/BooCatController.cs
@@ -34,13 +34,10 @@ public static class BooCatController
     public static async Task<BookInfo[]?> GetCached(IDistributedCache? cache, string provider, string name)
     {
         if (cache == null) return null;
-        Console.WriteLine($"CACHE-GET: {provider}-{name}");
         try
         {
             var result = await cache.GetStringAsync(GetCachedKey(provider, name));
             if (string.IsNullOrEmpty(result)) return null;
-            Console.WriteLine($"CACHE-GET: OK");
-            
             return JsonSerializer.Deserialize<BookInfo[]>(result);
         }
         catch
@@ -52,13 +49,9 @@ public static class BooCatController
     public static async Task<bool> SetCache<T>(IDistributedCache? cache, string provider, string name, T value)
     {
         if (cache == null) return false;
-        Console.WriteLine($"CACHE-SET: {provider}-{name}: {value}");
-        
         try
         {
             await cache.SetStringAsync(GetCachedKey(provider, name), JsonSerializer.Serialize(value), CacheOption);
-        Console.WriteLine($"CACHE-SET: OK");
-
             return true;
         }
         catch (Exception ex)
