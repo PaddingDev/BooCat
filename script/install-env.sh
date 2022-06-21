d03ab22dc06becb39e7cc039633a92033e5ab8d7
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-21 12:07:26 +0100

    feat: install redis

--- script/install-env.sh
+++ script/install-env.sh
@@ -14,3 +14,14 @@ sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-6.0
+
+## Install Redis
+
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+sudo apt-get update
+sudo apt-get install redis
+sudo systemctl enable redis
+sudo systemctl start redis
