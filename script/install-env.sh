ac8cb56ccf0670bbfc0626a216b7d68a9c589c98
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-21 12:11:59 +0100

    fix: redis

--- script/install-env.sh
+++ script/install-env.sh
@@ -22,6 +22,6 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 sudo apt-get update
-sudo apt-get install redis
-sudo systemctl enable redis
-sudo systemctl start redis
+sudo apt-get install redis -y
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
