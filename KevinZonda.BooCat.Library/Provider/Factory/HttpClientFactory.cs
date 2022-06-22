4014e8ccac1eec2b861dd2a4c5fd0ecb9271955c
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-22 01:04:34 +0100

    fix: hc

--- KevinZonda.BooCat.Library/Provider/Factory/HttpClientFactory.cs
+++ KevinZonda.BooCat.Library/Provider/Factory/HttpClientFactory.cs
@@ -3,11 +3,20 @@
     internal class HttpClientFatory
     {
         private static HttpClient? _httpClient = null;
-        
+
         public static HttpClient Produce()
         {
-            if (_httpClient == null) _httpClient = new HttpClient();
-            _httpClient.Timeout = TimeSpan.FromSeconds(7);
+            if (_httpClient == null)
+            {
+                _httpClient = new HttpClient();
+                _httpClient.Timeout = TimeSpan.FromSeconds(7);
+                _httpClient.DefaultRequestHeaders
+                           .UserAgent
+                           .ParseAdd(
+                               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                               "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                               "Chrome/102.0.0.0 Safari/537.36");
+            }
             return _httpClient;
         }
     }
